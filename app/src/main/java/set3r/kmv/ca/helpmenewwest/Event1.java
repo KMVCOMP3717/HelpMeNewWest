fb72ae39c8be7a8bd04e5658e51ac676e23089e6
Matthew Li <li.matthew.m@gmail.com>
2017-01-24 17:10:13 -0800

    Updating EventViewer adding test pag for Event1

---
+++ app/src/main/java/set3r/kmv/ca/helpmenewwest/Event1.java
@@ -0,0 +1,14 @@
+package set3r.kmv.ca.helpmenewwest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Event1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event1);
+        setTitle("Event Name");
+    }
+}
