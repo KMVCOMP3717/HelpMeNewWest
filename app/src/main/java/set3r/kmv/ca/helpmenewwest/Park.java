eae1c73678aae64b637e1ee570f4be5c5175057b
Vincent Lee <vincent.h.lee@outlook.com>
2017-01-26 00:16:30 -0800

    The Park Category with list view and park template page populated with placeholders.

--- app/src/main/java/set3r/kmv/ca/helpmenewwest/Park.java
+++ app/src/main/java/set3r/kmv/ca/helpmenewwest/Park.java
@@ -1,13 +1,34 @@
 package set3r.kmv.ca.helpmenewwest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Park extends AppCompatActivity {
+    Button park1;
+    Button park2;
+    Button park3;
+    Button park4;
+    Button parkList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_park);
+        park1 = (Button)findViewById(R.id.park1);
+        park2 = (Button)findViewById(R.id.park2);
+        park3 = (Button)findViewById(R.id.park3);
+        park4 = (Button)findViewById(R.id.park4);
+        parkList = (Button)findViewById(R.id.parklistbtn);
+    }
+
+    public void toParkListOnClick(View v) {
+        startActivity(new Intent(this, ParkList.class));
+    }
+
+    public void toParkTemplateOnClick(View v) {
+        startActivity(new Intent(this, ParkTemplate.class));
     }
 }
