258978638d77189b5aeff02a2a5bfc03fab3d214
Matthew Li <li.matthew.m@gmail.com>
2017-04-11 19:33:30 -0700

    why did I delete that, I don't know.

--- app/src/main/java/set3r/kmv/ca/helpmenewwest/DetailsView.java
+++ app/src/main/java/set3r/kmv/ca/helpmenewwest/DetailsView.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
